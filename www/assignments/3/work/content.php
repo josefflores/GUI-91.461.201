397259e7f4f3798fecdcabc02b0adfd29adf9e98
Jose F Flores <jose.flores.152@gmail.com>
2014-09-17 19:17:30 -0400

    Fixed indentation

--- www/assignments/3/work/content.php
+++ www/assignments/3/work/content.php
@@ -57,7 +57,9 @@
         -->
 
         <!-- Setting the page title-->
-        <title> GUI Programming I </title>
+        <title>
+            GUI Programming I
+        </title>
 
         <!-- Adding link to Assignment 3 css stylesheet file, This file has the styling for the example elements-->
         <link type="text/css" rel="stylesheet" href="<?php echo $A[ 'W_CSS' ] ; ?>assignment3.css">
@@ -91,20 +93,36 @@
                     <div class="header-right" >
 
                         <!-- Title of the page -->
-                        <h1 class="title">91.461 GUI Programming I</h1>
+                        <h1 class="title">
+                            91.461 GUI Programming I
+                        </h1>
 
                         <!-- Subtitle description -->
-                        <h2 class="title-sub">Fall 2014 Semester, Section 201</h2>
+                        <h2 class="title-sub">
+                            Fall 2014 Semester, Section 201
+                        </h2>
 
                     </div>
 
                     <!-- Navigation menu -->
                     <nav class="horizontal">
-                        <ul>
-                            <li><a href="<?php echo $A[ 'W_ROOT' ] ; echo 'assignments/3/' ?> "> Instructions </a></li>
 
-                            <li><a href="<?php echo $A[ 'W_ROOT' ] ; echo 'assignments/3/work/' ?>"> Work </a></li>
+                        <ul>
+
+                            <li>
+                                <a href="<?php echo $A[ 'W_ROOT' ] ; echo 'assignments/3/' ?> ">
+                                    Instructions
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="<?php echo $A[ 'W_ROOT' ] ; echo 'assignments/3/work/' ?>">
+                                    Work
+                                </a>
+                            </li>
+
                         </ul>
+
                     </nav>
                 <!-- End header wrapper-->
                 </div>
@@ -118,55 +136,168 @@
 
                 <!-- The meat of the assignment -->
                 <img class="description" src="<?php echo $A[ 'W_IMG' ] ; ?>assignment.png" alt="assignment logo" >
-                    <div class="description" >
-                        <h3>Assignment 3</h3>
-                        <h4>Text Formatting</h4>
-                        <p> This assignment is focused on how to manipulate text with css. </p>
-                    </div>
+                <div class="description" >
+                    <h3>
+                        Assignment 3
+                    </h3>
+                    <h4>
+                        Text Formatting
+                    </h4>
+                    <p>
+                        This assignment is focused on how to manipulate text with css.
+                    </p>
+                </div>
 
                     <!-- Explaining the lements to be used -->
                     <div class="description" >
-                        <h3> Elements</h3>
-                        <h4> Common text containers</h4>
+                        <h3>
+                            Elements
+                        </h3>
+                        <h4>
+                            Common text containers
+                        </h4>
+
                         <br/>
-                        <h5> Headings </h5>
+
+                        <h5>
+                            Headings
+                        </h5>
 
                         <!-- Explaining Headings -->
-                        <p> Heading tags range from h1 to h6, these tags are used to format text headings and are to be used sequentially; an h1 tag proceeds an h2 tag, an h2 tag preceeds an h3 tag, and so on. These tags are used to describe the information under them and can be precursors to other elements; the following element&apos;s content should fall under the category described by the preceding headings.<p>
-                        <pre>&lt;h1&gt;<h1 class="example-1">Heading 1 - The top level heading, usually reserved for a title.</h1>&lt;/h1&gt;</pre>
-                        <pre>&lt;h2&gt;<h2 class="example-1">Heading 2 - The second largest heading, usually reserved for a sub-title or a description of the preceding title.</h2>&lt;/h2&gt;</pre>
-                        <pre>&lt;h3&gt;<h3 class="example-1">Heading 3 - The third largest heading.</h3>&lt;/h3&gt;</pre>
-                        <pre>&lt;h4&gt;<h4 class="example-1">Heading 4 - The fourth largest heading.</h4>&lt;/h4&gt;</pre>
-                        <pre>&lt;h5&gt;<h5 class="example-1">Heading 5 - The fifth largest heading.</h5>&lt;/h5&gt;</pre>
-                        <pre>&lt;h6&gt;<h6 class="example-1">Heading 6 - The smallest heading.</h6>&lt;/h6&gt;</pre>
+                        <p>
+                            Heading tags range from h1 to h6, these tags are used to format text headings and are to be used sequentially; an h1 tag proceeds an h2 tag, an h2 tag preceeds an h3 tag, and so on. These tags are used to describe the information under them and can be precursors to other elements; the following element&apos;s content should fall under the category described by the preceding headings.
+                        <p>
+
+                        <pre>
+                            &lt;h1&gt;
+                            <h1 class="example-1">
+                                Heading 1 - The top level heading, usually reserved for a title.
+                            </h1>
+                            &lt;/h1&gt;
+                        </pre>
+
+                        <pre>
+                            &lt;h2&gt;
+                            <h2 class="example-1">
+                                Heading 2 - The second largest heading, usually reserved for a sub-title or a description of the preceding title.
+                            </h2>
+                            &lt;/h2&gt;
+                        </pre>
+
+                        <pre>
+                            &lt;h3&gt;
+                            <h3 class="example-1">
+                                Heading 3 - The third largest heading.
+                            </h3>
+                            &lt;/h3&gt;
+                        </pre>
+
+                        <pre>
+                            &lt;h4&gt;
+                            <h4 class="example-1">
+                                Heading 4 - The fourth largest heading.
+                            </h4>
+                            &lt;/h4&gt;
+                        </pre>
+
+                        <pre>
+                            &lt;h5&gt;
+                            <h5 class="example-1">
+                                Heading 5 - The fifth largest heading.
+                            </h5>
+                            &lt;/h5&gt;
+                        </pre>
+
+                        <pre>
+                            &lt;h6&gt;
+                            <h6 class="example-1">
+                                Heading 6 - The smallest heading.
+                            </h6>
+                            &lt;/h6&gt;
+                        </pre>
 
                         <!-- explaining paragraphs -->
-                        <h5> Paragraphs </h5>
+                        <h5>
+                            Paragraphs
+                        </h5>
                         <!-- lipsum content taken from http://www.lipsum.com/ -->
-                        <p> Paragraph tags are used to contain organized sections of text, they will wrap text by default. <p>
-                        <pre><p class="example-1"> &lt;p&gt; I am a a little paragraph I will have a bunch of gibberish as a my content. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum &lt;/p&gt;</p> </pre>
+                        <p>
+                            Paragraph tags are used to contain organized sections of text, they will wrap text by default.
+                        <p>
+
+                        <pre>
+                            <p class="example-1">
+                                &lt;p&gt; I am a a little paragraph I will have a bunch of gibberish as a my content. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum &lt;/p&gt;
+                            </p>
+                        </pre>
 
                         <!-- explaining span em and strong -->
-                        <h5> Spans, Emphasis and Strong </h5>
-                        <p> Span tags are used to mark sections of text within a parent so that these sections of text can then be formatted differently. They have no initial formatting and can be used to just segregate subsections of text.</p>
-                        <pre><p class="example-1"> &lt;p&gt; Text inside a &lt;span&gt; <span class="example-1"> parent to be formatted </span> &lt;/span&gt; differently. &lt;/p&gt; </p> </pre>
-                        <p> Emphasis tags are meant to give a selection of text emphasis, its purpose is to make the contained text stand out, it&apos;s default formatting makes it italicized. </p>
-                        <pre><p class="example-1"> &lt;p&gt; Text inside a parent to be formatted for &lt;em&gt;<em> emphasis. </em>&lt;/em&gt; &lt;/p&gt; </pre>
-                        <p> Strong tags are used to give weight to a selection of text, this tag is commonly associated with being bold. </p>
-                        <pre><p class="example-1" >&lt;p&gt; Text inside a parent that needs to carry &lt; <strong class="example-1">strong</strong> &gt;<strong class="example-1"> more weight. </strong>&lt;/strong&gt; &lt;/p&gt; </pre>
+                        <h5>
+                            Spans, Emphasis and Strong
+                        </h5>
+                        <p>
+                            Span tags are used to mark sections of text within a parent so that these sections of text can then be formatted differently. They have no initial formatting and can be used to just segregate subsections of text.
+                        </p>
+
+                        <pre>
+                            <p class="example-1">
+                                &lt;p&gt; Text inside a &lt;span&gt;
+                                <span class="example-1">
+                                    parent to be formatted
+                                </span>
+                                &lt;/span&gt; differently. &lt;/p&gt;
+                            </p>
+                        </pre>
+
+                        <p>
+                            Emphasis tags are meant to give a selection of text emphasis, its purpose is to make the contained text stand out, it&apos;s default formatting makes it italicized.
+                        </p>
+
+                        <pre>
+                            <p class="example-1">
+                                &lt;p&gt; Text inside a parent to be formatted for &lt;em&gt;
+                                <em class="example-1">
+                                    emphasis.
+                                </em>
+                                &lt;/em&gt; &lt;/p&gt;
+                            </p>
+                        </pre>
+
+                        <p>
+                            Strong tags are used to give weight to a selection of text, this tag is commonly associated with being bold.
+                        </p>
+
+                        <pre>
+                            <p class="example-1" >
+                                &lt;p&gt; Text inside a parent that needs to carry &lt;strong&gt;
+                                <strong class="example-1">
+                                    more weight.
+                                </strong>
+                                &lt;/strong&gt; &lt;/p&gt;
+                            </p>
+                        </pre>
 
                     </div>
 
                     <!-- Formatting Elements -->
                     <div class="description" >
-                        <h3> Example Styling </h3>
-                        <h4> Styling the Common text containers</h4>
+                        <h3>
+                            Example Styling
+                        </h3>
+                        <h4>
+                            Styling the Common text containers
+                        </h4>
 
                         <br/>
 
                         <!-- restore formatting with css to initial values assignment3.css-->
-                        <h5> Before </h5>
-                        <p> This section will show how the previous tags found under Elements look by default on your browser. </p>
+                        <h5>
+                            Before
+                        </h5>
+
+                        <p>
+                            This section will show how the previous tags found under Elements look by default on your browser.
+                        </p>
 
                         <div class="sample">
                             <h1 class="example-1">
@@ -228,7 +359,10 @@
                         </div>
 
                         <!-- Show ways to style for meaning assignment3.css -->
-                        <h5> After </h5>
+                        <h5>
+                            After
+                        </h5>
+
                         <p>
                             This section know shows the same markup with modified styles.
                         </p>
@@ -248,8 +382,8 @@
                         <div class="sample">
                             <h3 class="example-2">
                                 Subheading h3 tag strengthening the use of a word with font-weight: bold;
-                                </h3>
-                            </div>
+                            </h3>
+                        </div>
 
                         <!-- describing span usage -->
                         <div class="sample">
@@ -296,6 +430,7 @@
                         </div>
 
                     </div>
+
                 </div>
 
             <!-- End content -->
@@ -314,7 +449,9 @@
 
                     <!-- Github link where versioning code is being stored -->
                     <div class="notice">
-                        <a href="https://github.com/josefflores/GUI-91.461.201">Github Repository</a>
+                        <a href="https://github.com/josefflores/GUI-91.461.201">
+                            Github Repository
+                        </a>
                     </div>
 
                 <!-- End wrapper -->
