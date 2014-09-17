72f07e241a19ba7bf9fa531dd5914d6daf67e043
Jose F Flores <jose.flores.152@gmail.com>
2014-09-17 02:47:32 -0400

    Fix and Testing jenkins deployment

--- www/assignments/3/content.php
+++ www/assignments/3/content.php
@@ -14,9 +14,9 @@
      *  file has had its extension modified to work as a php file to
      *  allow for header redirects from unwanted access as well as to
      *  allow for dynamic includes as the css and image locations are not
-     *  hardcoded as they originally were. This assignments purpose is to 
-     * 	show knowledge of text styling with css, So I have created a quick 
-     * 	tutorial on how to format text.
+     *  hardcoded as they originally were. This assignments purpose is to
+     *  show knowledge of text styling with css, So I have created a quick
+     *  tutorial on how to format text.
      *
      *  9/10/14 Modified html to work as php
      */
@@ -113,27 +113,27 @@
             <!-- Begin content -->
             <div class="content">
 
-				<img class="description" src="<?php echo $A[ 'W_IMG' ] ; ?>assignment.png" alt="assignment logo" >
-				
-				<div class="description" >
-					<h3>Assignment 3</h3>
-					<h4>Instructions</h4>
-					
-					<p>
-						<a href="https://teaching.cs.uml.edu/~heines/91.461/91.461-2014-15f/461-assn/FirstCSS-v03.jsp">Assignment 3 Instructions - Styling Your First Web Page With CSS</a> 
-					</p>
-					
-					<ul>  
-						<li>Read up to page 68 in the textbook.</li>
-						<li>Read chapter 6 of the textbook</li>
-						<li>Did tutorial pages 178 - 188.</li>
-						<li>Watched lesson 3 of Build Your First Website: Getting Started with HTML & CSS by Kevin Yank </li>
-						<li>Watched Lessons 1 and 2 of Getting Started with CSS by Russ Weakley.  </li>
-						<li>Redid the <a href="">assignment 2 page</a> as a central site. The website now contains an expanding about section, an assignment repository, and a php source file viewer. This website is written in PHP to generate HTML; the HTML output of PHP has been written to maintain its indentation structure but since PHP generates reusable components, some items indentation might not perfectly align if subsections are reused in differently indented locations.  I also took the assignemnt 2 page and updated its styling to match the overall website and then used it to generate a two page sub-website that explains assignment 3 and what I did on the Instructions page. For the work page I added a small tutorial to show HTML text formatting as a way to meet the assignments text styling criteria.</li>
+                <img class="description" src="<?php echo $A[ 'W_IMG' ] ; ?>assignment.png" alt="assignment logo" >
+
+                <div class="description" >
+                    <h3>Assignment 3</h3>
+                    <h4>Instructions</h4>
+
+                    <p>
+                        <a href="https://teaching.cs.uml.edu/~heines/91.461/91.461-2014-15f/461-assn/FirstCSS-v03.jsp">Assignment 3 Instructions - Styling Your First Web Page With CSS</a>
+                    </p>
+
+                    <ul>
+                        <li>Read up to page 68 in the textbook.</li>
+                        <li>Read chapter 6 of the textbook</li>
+                        <li>Did tutorial pages 178 - 188.</li>
+                        <li>Watched lesson 3 of Build Your First Website: Getting Started with HTML & CSS by Kevin Yank </li>
+                        <li>Watched Lessons 1 and 2 of Getting Started with CSS by Russ Weakley.  </li>
+                        <li>Redid the <a href="">assignment 2 page</a> as a central site. The website now contains an expanding about section, an assignment repository, and a php source file viewer. This website is written in PHP to generate HTML; the HTML output of PHP has been written to maintain its indentation structure but since PHP generates reusable components, some items indentation might not perfectly align if subsections are reused in differently indented locations.  I also took the assignemnt 2 page and updated its styling to match the overall website and then used it to generate a two page sub-website that explains assignment 3 and what I did on the Instructions page. For the work page I added a small tutorial to show HTML text formatting as a way to meet the assignments text styling criteria.</li>
                     </ul>
-				
-				</div>
-                
+
+                </div>
+
             <!-- End content -->
             </div>
 
