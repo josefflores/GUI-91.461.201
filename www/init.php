7a555dd0c8bfa54c8eea2db6f8188ae2a0e6d54b
Jose F Flores <jose.flores.152@gmail.com>
2014-09-21 14:52:15 -0400

    added commenting

--- www/init.php
+++ www/init.php
@@ -1,5 +1,20 @@
 <?php
-	$root = '../json/roots.json' ;
+	 /**
+     *  @file   init.php
+     *  @author Jose F. Flores <jose_flores@student.uml.edu>
+     *
+     *  Copyright (c) 2014 by Jose F. Flores. All rights reserved. May be
+     *      freely copied or excerpted for educational purposes with credit
+     *      to the author.
+     *
+     *  A Umass Lowell Computer Science Student 91.461 
+     *
+     *  This file holds the php init script for the application
+     *
+     *  9/21/14 	Created init script
+     */
+     
+    $root = '../json/roots.json' ;
 	
 	if ( is_file( $root ) ) {
 		
